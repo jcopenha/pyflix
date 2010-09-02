2260949da3ac832b6878b7391a51bc3e928ba424
jcopenha <jcopenha@typedef.org>
2010-09-01 20:44:30 -0400

    they at least FAIL instead of erroring out

--- test.py
+++ test.py
@@ -48,49 +48,48 @@ EXAMPLE_USER = {
         }
 }
 
-class TestQuery():
+class TestQuery(unittest.TestCase):
 	def test_base(self):
 		netflixClient = NetflixClient(APP_NAME, API_KEY, API_SECRET, CALLBACK)
 		
-	def test_token_functions(self):
-		netflixClient = NetflixClient(APP_NAME, API_KEY, API_SECRET, CALLBACK)
-		netflixUser = NetflixUser(EMPTY_USER,netflixClient)
+#	def test_token_functions(self):
+#		netflixClient = NetflixClient(APP_NAME, API_KEY, API_SECRET, CALLBACK)
+#		netflixUser = NetflixUser(EMPTY_USER,netflixClient)
 		# I'd love to test the token functions, but unfortunately running these
 		# invalidates the existing tokens.  Foo.
 
 		
 	def test_catalog_functions(self):
 		netflixClient = NetflixClient(APP_NAME, API_KEY, API_SECRET, CALLBACK)
-		data = netflixClient.catalog.searchStringTitles('Foo')
+		data = netflixClient.catalog.autocomplete('Foo')
 		for info in data:
-			assert re.search('Foo',info['title']['short'])
+			assert re.search('Foo',info)
 			
-		data = netflixClient.catalog.searchTitles(MOVIE_TITLE)
-		assert isinstance(data[0]['title']['regular'],unicode)
+		data = netflixClient.catalog.search_titles(MOVIE_TITLE)
+		assert isinstance(data[0].title,unicode)
 		
-		movie = netflixClient.catalog.getTitle(data[0]['id'])
+		movie = netflixClient.catalog.index(data[0].id)
 		assert isinstance(movie['catalog_title']['title']['regular'],unicode)
 		
-		people = netflixClient.catalog.searchPeople('Flip Wilson',maxResults=1)
+		people = netflixClient.catalog.search_people('Flip Wilson',maxResults=1)
 		assert isinstance(people,dict)
 		
 	# DISC TESTS
 	def test_disc_functions(self):
 		netflixClient = NetflixClient(APP_NAME, API_KEY, API_SECRET, CALLBACK)
-		data = netflixClient.catalog.searchTitles('Cocoon',1,2)
-		testSubject = data[0]
-		disc = NetflixTitle(testSubject,netflixClient)
-		formats = disc.getInfo('formats')
+		titles = netflixClient.catalog.search_titles('Cocoon',1,2)
+		formats = titles[0].formats()
 		assert isinstance(formats,dict)
-		synopsis = disc.getInfo('synopsis')
-		assert isinstance(synopsis,dict)
+#TODO : not implemented anymore?
+#		synopsis = disc.getInfo('synopsis')
+#		assert isinstance(synopsis,dict)
 				
 	def test_user_functions(self):
 		netflixClient = NetflixClient(APP_NAME, API_KEY, API_SECRET, CALLBACK)
 		netflixUser = NetflixUser(EXAMPLE_USER,netflixClient)
-		user = netflixUser.getData()
-		assert isinstance(user['first_name'],unicode)
-		data = netflixClient.catalog.searchTitles('Cocoon',1,2)
+		username = netflixUser.name
+		assert isinstance(username,unicode)
+		data = netflixClient.catalog.search_titles('Cocoon',1,2)
 		ratings = netflixUser.getRatings(data)
 		history = netflixUser.getRentalHistory('shipped',updatedMin=1219775019,maxResults=4)
 		assert int(history['rental_history']['number_of_results']) <= 5
